380257fd06751d8d3c1e6204a9e73679d7e91154
Rushan <supercow7750@outlook.com>
2020-06-14 17:38:48 +0530

    first commit

---
+++ releases/files/setup.sh
@@ -0,0 +1,21 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y update
+sudo apt-get -y update
+sudo apt-get -y dist-upgrade
+sudo apt-get install -y rpi-chromium-mods
+sudo apt-get install -y python-sense-emu python3-sense-emu
+sudo apt-get install -y python-sense-emu-doc realvnc-vnc-viewer
+sudo apt-get -y update
+sudo raspi-config 
+sudo apt clean
+sudo sudo apt-get remove apt-listchanges
+sudo apt autoremove -y
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo apt autoclean
+sudo apt clean
+sudo apt autoremove
+sudo reboot now
